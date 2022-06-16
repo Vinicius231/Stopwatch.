b990b3504e1f486b233305a785395e1b74cce185
Vinicius231 <103453815+Vinicius231@users.noreply.github.com>
2022-06-16 10:31:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-var horas = 00
-var minutos = 00 
-var segundos = 00 
+var hora = 0
+var minuto = 0
+var segundo = 0
 
-var second = document.getElementById('segundo')
-var hour = document.getElementById('hora')
-var button = document.getElementById('iniciar')
-
+var btniniciar = document.getElementById('iniciar')
 var interval
 
-function start(){
-    interval = setInterval(contador,1000)
-    button.style.visibility='hidden'
-
-}
-function pause(){
-    clearInterval(interval)
-    button.style.visibility=''
+function iniciar(){
+    interval = setInterval(começar,1000)
+    btniniciar.style.visibility = 'hidden'
 }
 
-function stoping(){
-    location.reload()
-}
-
-function contador() {
-    segundos++
-    if (segundos == 60) {
-        minutos++
-        segundos = 0
-        if (minutos < 10) {minutos = '0'+minutos}
-        document.getElementById('minuto').innerHTML=`${minutos}:`
-        if (minutos == 60) {
-            horas++
-            minutos = 0
-            segundos = 0
-            if (horas < 10) {horas = '0'+horas}
-            document.getElementById('hora').innerHTML=`${horas}:`
-
-        } 
+function começar(){
+    segundo++
+    if(segundo<10){segundo='0'+segundo}
+    document.getElementById('segundos').innerHTML = segundo
+    if(segundo == 60){
+        segundo = 0
+        minuto++
+        if(minuto<10){minuto = '0'+minuto}
+        if(segundo<10){segundo = '0'+segundo}
+        document.getElementById('minutos').innerHTML = minuto+':'
+        document.getElementById('segundos').innerHTML = segundo
+    }
+    if(minuto == 60){
+        segundo = -1
+        minuto = 0
+        hora++
+        if(hora<10){hora='0'+hora}
+        if(minuto<10){minuto='0'+minuto}
+        document.getElementById('horas').innerHTML = hora+':'
+        document.getElementById('minutos').innerHTML = minuto+':'
     }
     
-    if (segundos < 10){
-        segundos = '0'+segundos
-    }
-    var seg = second.innerHTML = segundos
+}
+function pausar(){
+    clearInterval(interval)
+    btniniciar.style.visibility = ''
+}
+function parar(){
+    clearInterval(interval)
+    btniniciar.style.visibility = ''
+    hora = 0
+    minuto = 0
+    segundo = 0
+    document.getElementById('horas').innerHTML = '00:'
+    document.getElementById('minutos').innerHTML = '00:'
+    document.getElementById('segundos').innerHTML = '00'
 }
